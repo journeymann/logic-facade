8ab8b67b6bd6eb64014de238542fc36e22eaa8ef
Casmon Gordon <48144124+journeymann@users.noreply.github.com>
2019-03-05 12:15:31 -0500

    Create BaseEntity.java

---
+++ com/research/innovate/patterns/nullobject/BaseEntity.java
@@ -0,0 +1,15 @@
+com.research.innovate.patterns.nullobject
+
+/**
+ * @author cgordon
+ * @created 03/05/2019
+ * @version 1.0
+ * 
+ * Null Object design pattern implementation. [BaseEntity Interface]
+ *
+ */
+
+interface BaseEntity{
+
+    boolean isNull();
+}
