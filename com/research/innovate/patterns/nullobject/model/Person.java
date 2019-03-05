7036755fa8e8cdb464e7cc14dea514ae8b043dfa
Casmon Gordon <48144124+journeymann@users.noreply.github.com>
2019-03-05 12:19:42 -0500

    Create Person.java

---
+++ com/research/innovate/patterns/nullobject/model/Person.java
@@ -0,0 +1,17 @@
+com.research.innovate.patterns.nullobject.model
+
+import com.research.innovate.patterns.nullobject.*
+
+/**
+ * @author cgordon
+ * @created 03/05/2019
+ * @version 1.0
+ * 
+ * Null Object design pattern implementation.
+ *
+ */
+
+public abstract class Person extends Entity{
+
+    String name, address;
+}
